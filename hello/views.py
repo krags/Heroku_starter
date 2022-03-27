e924153451a85839dd792777fb678a2ab007aa67
Keith Ragsdale <keithrags@gmail.com>
2022-03-26 23:32:11 -0700

    test

--- hello/views.py
+++ hello/views.py
@@ -6,9 +6,10 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # r = requests.get('https://httpbin.org/status/418')
+    # print(r.text)
+    # return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('<h1>My stuff</h1>')
 
 
 def db(request):
